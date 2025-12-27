cc04d7f3566b44af58db93de07845ee019e58c43
Ram Kumar <ramk587@gmail.com>
2025-12-27 20:27:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -134,5 +134,8 @@ def save_result():
     return "✅ Result saved successfully!"
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
